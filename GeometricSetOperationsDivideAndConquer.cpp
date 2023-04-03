ddac191f001f01ded4a22a549f4939686b2487d8
Oscar Rodas <60824325+thescar101@users.noreply.github.com>
2023-04-03 18:48:07 -0400

    Intersection of Points2D DC works, also added more tests
    These tests should be replicated for Plane sweep versions

--- GeometricSetOperationsDivideAndConquer.cpp
+++ GeometricSetOperationsDivideAndConquer.cpp
@@ -18,8 +18,12 @@ std::vector<SimplePoint2D>::iterator BinarySplitX(std::vector<SimplePoint2D>::it
         {
             return BinarySplitX(begin, mid, split);
         }
-        else
+        else // mid->x == split
+        {
+            while (mid->x == split)
+                mid++;
             return mid;
+        }
     }
 }
 std::vector<SimplePoint2D>::iterator BinarySplitY(std::vector<SimplePoint2D>::iterator begin, std::vector<SimplePoint2D>::iterator end, const Number& split)
@@ -39,8 +43,13 @@ std::vector<SimplePoint2D>::iterator BinarySplitY(std::vector<SimplePoint2D>::it
         {
             return BinarySplitY(begin, mid, split);
         }
-        else
+        else // mid->y == split
+        {
+            while (mid->y == split)
+                mid++;
             return mid;
+        }
+            
     }
 }
 
@@ -79,6 +88,21 @@ void CompareIntersection(std::vector<SimplePoint2D>::iterator aBegin, std::vecto
 
     if ((yMax - yMin) < zero) // Range has no possible intersection
         return;
+
+    // if (aBegin->y != yMin)
+    //     aBegin = BinarySplitY(aBegin, aEnd, yMin);
+    // if (bBegin->y != yMin)
+    //     bBegin = BinarySplitY(bBegin, bEnd, yMin);
+
+    // if (aBack->y != yMax)
+    //     aEnd = BinarySplitY(aBegin, aEnd, yMax);
+    // if (bBack->y != yMax)
+    //     bEnd = BinarySplitY(bBegin, bEnd, yMax);
+
+    // aBack = aEnd;
+    // aBack--;
+    // bBack = bEnd;
+    // bBack--;
     
     while (aBegin->y < yMin)
         aBegin++;
@@ -98,13 +122,13 @@ void CompareIntersection(std::vector<SimplePoint2D>::iterator aBegin, std::vecto
     Number yRange = yMax - yMin;
     Number split = (yRange/two) + yMin; 
 
-    // Base Case (xRange == 0)
-    
-    if (yRange == zero) // We have found an intersection point
-    {
+    // Base Case (yRange == 0), potential intersection
+    if (aBegin->y == bBack->y && aBack->y == bBegin->y) // We have found an intersection point
+     {
         intersectionPoints.push_back(*aBegin);
         return;
     }
+    
     
     // Recurse Case
     std::vector<SimplePoint2D>::iterator aSplit = BinarySplitY(aBegin, aEnd, split);
@@ -149,6 +173,21 @@ void DivideAndConquer(std::vector<SimplePoint2D>::iterator aBegin, std::vector<S
     if ((xMax-xMin) < zero) // Range has no possible intersection
         return;
     
+    // if (aBegin->x != xMin)
+    //     aBegin = BinarySplitX(aBegin, aEnd, xMin);
+    // if (bBegin->x != xMin)
+    //     bBegin = BinarySplitX(bBegin, bEnd, xMin);
+
+    // if (aBack->x != xMax)
+    //     aEnd = BinarySplitX(aBegin, aEnd, xMax);
+    // if (bBack->x != xMax)
+    //     bEnd = BinarySplitX(bBegin, bEnd, xMax);
+
+    // aBack = aEnd;
+    // aBack--;
+    // bBack = bEnd;
+    // bBack--;
+
     while (aBegin->x < xMin)
         aBegin++;
     while (bBegin->x < xMin)
@@ -190,6 +229,7 @@ Point2D IntersectionDC(const Point2D& a, const Point2D& b)
     std::vector<SimplePoint2D> intersectionPoints;
     std::vector<SimplePoint2D> pointCollectionA;
     std::vector<SimplePoint2D> pointCollectionB;
+    
     for (Point2D::Iterator itr = a.begin(); itr != a.end(); itr++)
     {
         pointCollectionA.push_back(*itr);
--- main.cpp
+++ main.cpp
@@ -57,11 +57,158 @@ void test_GeomSetOps1()
     }
 }
 
+void test_GeomSetOps2()
+{
+    std::cout << "Testing Geometric Set Operations" << std::endl;
+
+    std::vector<SimplePoint2D> pointsA;
+    std::vector<SimplePoint2D> pointsB;
+    std::vector<SimplePoint2D> intersectionPoints;
+
+    Number x = "1.0", y = "1.0", multiple = "1.0";
+
+    for (int i = 0; i < 100; i++)
+    {
+        y = "1.0";
+        for (int j = 0; j < 100; j++)
+        {
+            int k = (i * 100) + j;
+            if (k % 2 == 0)
+            {
+                pointsA.push_back(SimplePoint2D(x, y));
+            }
+            if (k % 5 == 0)
+            {
+                pointsB.push_back(SimplePoint2D(x, y));
+            }
+            if (k % 2 == 0 && k % 5 == 0)
+            {
+                intersectionPoints.push_back(SimplePoint2D(x, y));
+            }
+            y += multiple;
+        }
+        x += multiple;
+    }
+
+    Point2D A(pointsA);
+    Point2D B(pointsB);
+    Point2D C = IntersectionDC(A, B);
+
+    std::vector<SimplePoint2D> pointsC;
+    for (auto itr = C.begin(); itr != C.end(); itr++)
+    {
+        pointsC.push_back(*itr);
+    }
+
+    std::cout << std::endl;
+
+    bool isEqual = true;
+    if (pointsC.size() == intersectionPoints.size())
+    {
+        for (int i = 0; i < pointsC.size(); i++)
+        {
+            if (pointsC.at(i) != intersectionPoints.at(i))
+            {
+                isEqual = false;
+                std::cout << std::endl;
+                std::cout << "(" << pointsC.at(i).x << ", " << pointsC.at(i).y << ")" << std::endl;
+                std::cout << "(" << intersectionPoints.at(i).x << ", " << intersectionPoints.at(i).y << ")" << std::endl;
+                std::cout << std::endl;
+            }
+        }
+    }
+    else
+    {
+        isEqual = false;
+        std::cout << "Points C Size: " << pointsC.size() << std::endl;
+        std::cout << "Intersection Points: " << intersectionPoints.size() << std::endl;
+    }
+
+    if (isEqual)
+        std::cout << "Intersection correctly found" << std::endl;
+    else
+        std::cout << "Intersection failed" << std::endl;
+}
+
+void test_GeomSetOps3()
+{
+    std::cout << "Testing Geometric Set Operations" << std::endl;
+
+    std::vector<SimplePoint2D> pointsA;
+    std::vector<SimplePoint2D> pointsB;
+    std::vector<SimplePoint2D> intersectionPoints;
+
+    Number x = "1.0", y = "1.0", multiple = "1.0";
+
+    for (int i = 0; i < 100; i++)
+    {
+        y = "1.0";
+        for (int j = 0; j < 100; j++)
+        {
+            int k = (i * 100) + j;
+            if ((k % 2 == 0 || k % 5 == 0) && k % 3 == 0)
+            {
+                pointsA.push_back(SimplePoint2D(x, y));
+            }
+            if ((k % 2 == 0 || k % 5 == 0) && k % 7 == 0)
+            {
+                pointsB.push_back(SimplePoint2D(x, y));
+            }
+            if ((k % 2 == 0 || k % 5 == 0) && (k % 3 == 0 && k % 7 == 0))
+            {
+                intersectionPoints.push_back(SimplePoint2D(x, y));
+            }
+            y += multiple;
+        }
+        x += multiple;
+    }
+
+    Point2D A(pointsA);
+    Point2D B(pointsB);
+    Point2D C = IntersectionDC(A, B);
+
+    std::vector<SimplePoint2D> pointsC;
+    for (auto itr = C.begin(); itr != C.end(); itr++)
+    {
+        pointsC.push_back(*itr);
+    }
+    
+    std::cout << std::endl;
+
+    bool isEqual = true;
+    if (pointsC.size() == intersectionPoints.size())
+    {
+        for (int i = 0; i < pointsC.size(); i++)
+        {
+            if (pointsC.at(i) != intersectionPoints.at(i))
+            {
+                isEqual = false;
+                std::cout << std::endl;
+                std::cout << "(" << pointsC.at(i).x << ", " << pointsC.at(i).y << ")" << std::endl;
+                std::cout << "(" << intersectionPoints.at(i).x << ", " << intersectionPoints.at(i).y << ")" << std::endl;
+                std::cout << std::endl;
+            }
+        }
+    }
+    else
+    {
+        isEqual = false;
+        std::cout << "Points C Size: " << pointsC.size() << std::endl;
+        std::cout << "Intersection Points: " << intersectionPoints.size() << std::endl;
+    }
+
+    if (isEqual)
+        std::cout << "Intersection correctly found" << std::endl;
+    else
+        std::cout << "Intersection failed" << std::endl;
+}
+
 int main (void)
 {
 
     test_GeomSetOps1();
     test_GeomSetOps2();
+    test_GeomSetOps3();
 
     return 0;
 }
